33164825d64ae8f63efa174829880e1a64251636
kibaota <ohta.syunsuke@liveout.co.jp>
2021-01-11 23:39:06 +0900

    ランダムで色を出力した。 DARKかLIGHTかを選択できるようにした。 NEXTボタンを押すとアラートするようにした。

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -6,23 +6,21 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-card>
+        <ion-title class="bigbig">Color Darkness<br>Cheker</ion-title>
+        <section>
+          <ion-button size="large" routerLink="tab2">Classify Darkness</ion-button>
+        </section>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard } from '@ionic/vue';
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonCard }
 }
 </script>
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -6,23 +6,55 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <ion-card>
+        <ion-label>
+          <div v-bind:style="{background:setRandomColor()}" class="box color_sample"></div>
+        </ion-label>
+        <ion-segment value="all">
+          <ion-segment-button value="all">
+            <ion-label>Dark</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="favorites">
+            <ion-label>Light</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </ion-card>
+      <ion-button v-on:click="nextColor()">
+        Next
+        <ion-icon :icon="arrowForwardOutline" />
+      </ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonSegment, IonIcon } from '@ionic/vue';
+import { arrowForwardOutline } from 'ionicons/icons';
+
 
 export default  {
   name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  methods: {
+    segmentChanged(ev: CustomEvent) {
+      console.log('Segment changed', ev);
+    },
+    setRandomColor() {
+      const R = Math.floor(Math.random() * 255);
+      const G = Math.floor(Math.random() * 255);
+      const B = Math.floor(Math.random() * 255);
+      const RGB = "RGB(" + R + "," + G + "," + B + ")";
+      return RGB
+    },
+    nextColor() {
+      alert('3');
+      
+    }
+  },
+  setup() {
+    return {
+      arrowForwardOutline
+    }
+  },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCard, IonSegment, IonIcon }
 }
 </script>
